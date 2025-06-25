46791292285d6883ed94ffcd564178bad9735317
Kevin Mallari <kevinmallari62@gmail.com>
2025-06-25 23:36:44 +0800

    feat(packages): support zod v4

--- src/middlewares/on-error.ts
+++ src/middlewares/on-error.ts
@@ -1,5 +1,5 @@
 import type { ErrorHandler } from "hono";
-import type { StatusCode } from "hono/utils/http-status";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
 
 import { INTERNAL_SERVER_ERROR, OK } from "../http-status-codes.js";
 
@@ -8,7 +8,7 @@ const onError: ErrorHandler = (err, c) => {
     ? err.status
     : c.newResponse(null).status;
   const statusCode = currentStatus !== OK
-    ? (currentStatus as StatusCode)
+    ? (currentStatus as ContentfulStatusCode)
     : INTERNAL_SERVER_ERROR;
   // eslint-disable-next-line node/prefer-global/process
   const env = c.env?.NODE_ENV || process.env?.NODE_ENV;
