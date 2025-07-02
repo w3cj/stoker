8a3edcf1166c0f4473fc0627192b906029625728
w3cj <cj@null.computer>
2025-07-02 10:01:22 -0600

    :bug: fix favicon content-type header

--- src/middlewares/serve-emoji-favicon.ts
+++ src/middlewares/serve-emoji-favicon.ts
@@ -3,7 +3,7 @@ import type { MiddlewareHandler } from "hono";
 const serveEmojiFavicon = (emoji: string): MiddlewareHandler => {
   return async (c, next) => {
     if (c.req.path === "/favicon.ico") {
-      c.header("Content-Type", "image/svg+xml");
+      c.res.headers.set("content-type", "image/svg+xml");
       return c.body(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" x="-0.1em" font-size="90">${emoji}</text></svg>`);
     }
     return next();
